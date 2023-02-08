594291398baee6d87093e597e11a7b2828abbe54
Frank Schmidt <frank.schmidt@codecentric.de>
2023-02-08 21:37:27 +0100

    Write RSS output to file

--- generate_rss.sh
+++ generate_rss.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 URL="https://www.backerkit.com/c/greater-than-games/spirit-island-nature-incarnate/updates#top"
 
+OUTDIR=${1:?missing parameter outdir}
+mkdir -p $OUTDIR
+
 curl -o mkfeed.py "https://raw.githubusercontent.com/dburic/mkfeed/master/mkfeed.py"
 sha256sum -c <<<"e043624cfa0005559d0a37d697af7c00fd7e9388cac70aa3329748b48e38ff0a  mkfeed.py"
 chmod +x mkfeed.py
@@ -16,6 +19,6 @@ curl $URL | ./mkfeed.py \
     --feed-desc 'Spirit Island Nature Incarnate Updates' \
     --item-title '{%2}' \
     --item-link "$(cut -d/ -f1,2,3 <<<$URL){%1}" \
-    --item-desc '{%2}'
+    --item-desc '{%2}' >$OUTDIR/my.rss
 
 
