3eb1f9b1a8d3e6aa1a05113bd57f951c23d989dd
Frank Schmidt <Gellardo@users.noreply.github.com>
2023-06-23 12:42:37 +0000

    Add scraping for tl;dr sec + automatic list entry generation

--- generate_rss.sh
+++ generate_rss.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-URL="https://www.backerkit.com/c/greater-than-games/spirit-island-nature-incarnate/updates#top"
-
 OUTDIR=${1:?missing parameter outdir}
 mkdir -p $OUTDIR
 
@@ -12,14 +10,35 @@ sha256sum -c <<<"e043624cfa0005559d0a37d697af7c00fd7e9388cac70aa3329748b48e38ff0
 chmod +x mkfeed.py
 patch <mkfeed.py.patch
 
-cp index.html $OUTDIR/index.html
+URL="https://www.backerkit.com/c/greater-than-games/spirit-island-nature-incarnate/updates#top"
 curl $URL | ./mkfeed.py \
     --pattern-item '<div class="relative">{*}<a{*}href="{%}"{*}>{*}<div class="flex-auto ">{%}</div>' \
     --feed-title 'Nature Incarnate' \
     --feed-link "$URL" \
     --feed-desc 'Spirit Island Nature Incarnate Updates' \
     --item-title '{%2}' \
-    --item-link "$(cut -d/ -f1,2,3 <<<$URL){%1}" \
-    --item-desc '{%2}' >$OUTDIR/nature-incarnate.rss
+    --item-link "$(cut -d/ -f1,2,3 <<<"$URL"){%1}" \
+    --item-desc '{%2}' >"$OUTDIR"/nature-incarnate.rss
 
+URL="https://tldrsec.com"
+curl "$URL" | ./mkfeed.py \
+    --pattern-main '<div class="col-span-12 w-full lg:col-span-9">{%}' \
+    --pattern-item '<a{*}href="{%}"{*}>{*}<div{*}><h2 {*}>{%}</h2>{%}</div>' \
+    --feed-title 'TL;DR Sec' \
+    --feed-link "$URL" \
+    --feed-desc 'TL;DR Sec does not have an rss feed anymore :(' \
+    --item-title '{%2}' \
+    --item-link "$(cut -d/ -f1,2,3 <<<"$URL"){%1}" \
+    --item-desc '{%3}' >"$OUTDIR"/tldr.rss
 
+# look for the first title tag in each rss file
+# then generate <li> for the index.html
+GENERATED_LI_ENTRIES=$(cd "$OUTDIR"; find . -name '*.rss' -exec awk '
+       /title>/ {printf "%s: %s\n", FILENAME, $0; exit}
+       ' {} \; |
+  sed 's_</\?title>__g;
+       s_^_    <li><a href="_;
+       s_:[ ]\+_">_;
+       s_$_</a></li>_;')
+export GENERATED_LI_ENTRIES
+envsubst <index.html >"$OUTDIR"/index.html
