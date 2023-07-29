e597dc065131e6774f0ff10ecf0761e904f5dd70
Frank Schmidt <Gellardo@users.noreply.github.com>
2023-07-29 21:34:10 +0000

    Consistent cleaning of rss

--- generate_rss.sh
+++ generate_rss.sh
@@ -9,6 +9,13 @@ curl -o mkfeed.py "https://raw.githubusercontent.com/dburic/mkfeed/master/mkfeed
 sha256sum -c <<<"96612dbe0c2c3a6b8b8a409846b621bf129aeb1d9b8155a7ca3ed3e755f0246b  mkfeed.py"
 chmod +x mkfeed.py
 
+clean_rss() {
+    # remove constantly updated values
+    file=$1
+    sed -i 's/[0-9an]\+ \(hour\|day\|week\|month\)s\? ago//' "$file"
+    sed -i 's/[0-9a-zA-Z]\{20,\}/hex-string/' "$file"
+}
+
 URL="https://www.backerkit.com/c/greater-than-games/spirit-island-nature-incarnate/community?filter=Crowdfunding%3A%3AProjectUpdate"
 curl $URL | ./mkfeed.py \
     --pattern-item '<div id="body" {*}>{*}<a{*}href="{%}"{*}>{*}<p{*}>{%}</p>{*}</a>{*}<div class="trix-content">{%}</div>' \
@@ -18,7 +25,7 @@ curl $URL | ./mkfeed.py \
     --item-title '{%2}' \
     --item-link "$(cut -d/ -f1,2,3 <<<"$URL"){%1}" \
     --item-desc '{%3}' >"$OUTDIR"/nature-incarnate.rss
-sed -i 's/[0-9an]\+ \(hour\|day\|week\|month\)s\? ago//' "$OUTDIR"/nature-incarnate.rss # remove constantly updated value
+clean_rss "$OUTDIR"/nature-incarnate.rss
 
 URL="https://tldrsec.com"
 curl "$URL" | ./mkfeed.py \
@@ -30,7 +37,7 @@ curl "$URL" | ./mkfeed.py \
     --item-title '{%2}' \
     --item-link "$(cut -d/ -f1,2,3 <<<"$URL"){%1}" \
     --item-desc '{%3}' >"$OUTDIR"/tldr.rss
-sed -i 's/[0-9an]\+ \(hour\|day\|week\|month\)s\? ago//' "$OUTDIR"/tldr.rss # remove constantly updated value
+clean_rss "$OUTDIR"/tldr.rss # remove constantly updated value
 
 # look for the first title tag in each rss file
 # then generate <li> for the index.html
